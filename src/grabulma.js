d5d041019859f43deb34efff8cb5b6213e0480c3
Juan Carlos <JuanCarlospaco@gmail.com>
2020-09-26 20:42:13 -0300

    init

--- src/grabulma.js
+++ src/grabulma.js
@@ -1730,6 +1730,73 @@ export default (editor, opts = {}) => {
     },
   });
 
+  plugin('bulma_buttons_grid', {
+    label:    'Buttons Grid 3x3',
+    content: `
+    <div class="buttons has-addons">
+      <button class="button is-success is-selected">Yes</button>
+      <button class="button">Maybe</button>
+      <button class="button">No</button>
+    </div>
+    <div class="buttons has-addons">
+      <button class="button">Yes</button>
+      <button class="button is-info is-selected">Maybe</button>
+      <button class="button">No</button>
+    </div>
+    <div class="buttons has-addons">
+      <button class="button">Yes</button>
+      <button class="button">Maybe</button>
+      <button class="button is-danger is-selected">No</button>
+    </div>`,
+    category: 'Bulma➡️Buttons',
+    media:    '<img src="bulma.svg" height=32 width=32 loading="lazy">',
+    render:   ({ el }) => {
+      const a = document.createElement('a');
+      a.target = '_blank';
+      a.innerHTML = '?';
+      a.title = 'Docs';
+      a.href = 'https://bulma.io/documentation/elements/button';
+      el.appendChild(a);
+    },
+  });
+
+  plugin('bulma_buttons_list', {
+    label:    'Buttons List',
+    content: `
+    <div class="buttons">
+      <button class="button">One</button>
+      <button class="button">Two</button>
+      <button class="button">Three</button>
+      <button class="button">Four</button>
+      <button class="button">Five</button>
+      <button class="button">Six</button>
+      <button class="button">Seven</button>
+      <button class="button">Eight</button>
+      <button class="button">Nine</button>
+      <button class="button">Ten</button>
+      <button class="button">Eleven</button>
+      <button class="button">Twelve</button>
+      <button class="button">Thirteen</button>
+      <button class="button">Fourteen</button>
+      <button class="button">Fifteen</button>
+      <button class="button">Sixteen</button>
+      <button class="button">Seventeen</button>
+      <button class="button">Eighteen</button>
+      <button class="button">Nineteen</button>
+      <button class="button">Twenty</button>
+    </div>`,
+    category: 'Bulma➡️Buttons',
+    media:    '<img src="bulma.svg" height=32 width=32 loading="lazy">',
+    render:   ({ el }) => {
+      const a = document.createElement('a');
+      a.target = '_blank';
+      a.innerHTML = '?';
+      a.title = 'Docs';
+      a.href = 'https://bulma.io/documentation/elements/button';
+      el.appendChild(a);
+    },
+  });
+
 
   /* Bulma Files *********************************************************/
 
