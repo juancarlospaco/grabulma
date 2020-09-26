505ad5fc9e8bfabcb2c0c85eb3746523e4d301ee
Juan Carlos <JuanCarlospaco@gmail.com>
2020-09-26 18:08:45 -0300

    init

--- src/index.js
+++ src/index.js
@@ -8,14 +8,13 @@ export default (editor, options = {}) => {
     editor.addComponents(
       `
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-      <script src="https://use.fontawesome.com/releases/v5.14.0/js/all.js" async defer ></script>
       `,
       {at: 0}
     );
 
     const openBl = editor.Panels.getButton('views', 'open-blocks');
     openBl && openBl.set('active', 1);
-    editor.Blocks.getCategories().filter(c => ['Spectre➡️Templates', 'SVG➡️Patterns', 'Fonts', 'Nim lang➡️SCF', 'Unsplash➡️Images', 'Spectre➡️Icons', 'Spectre➡️Buttons', 'Spectre➡️Experimentals', 'Spectre➡️Utils', 'Spectre➡️Media'].includes(c.get('id'))).forEach(c => c.set('open', 0));
+    //editor.Blocks.getCategories().filter(c => ['Spectre➡️Templates', 'SVG➡️Patterns', 'Fonts', 'Nim lang➡️SCF', 'Unsplash➡️Images', 'Spectre➡️Icons', 'Spectre➡️Buttons', 'Spectre➡️Experimentals', 'Spectre➡️Utils', 'Spectre➡️Media'].includes(c.get('id'))).forEach(c => c.set('open', 0));
 
   })
 
@@ -28,34 +27,34 @@ export default (editor, options = {}) => {
     id: 'open-code'
   }]);
 
-  editor.Panels.addPanel({id: 'options'}).get('buttons').add([{
-    attributes: {
-      title: 'Open Templates'
-    },
-    className: 'fa fa-paperclip',
-    command: 'open-templates',//Open modal with templates which can be used when creating new page
-    id: 'open-templates'
-  }, {
-    attributes: {
-      title: 'Open Pages'
-    },
-    className: 'fa fa-file-o',
-    command: 'open-pages',//Open modal listing your pages
-    id: 'open-pages'
-  }, {
-    attributes: {
-      title: 'Save As Template'
-    },
-    className: 'fa fa-archive',
-    command: 'save-as-template',//Save page as template
-    id: 'save-as-template'
-  }, {
-    attributes: {
-      title: 'Delete from IDB'
-    },
-    className: 'fa fa-trash-o',
-    command: 'delete-from-idb',//Delete open page or template
-    id: 'delete-from-idb'
-  }]);
+  // editor.Panels.addPanel({id: 'options'}).get('buttons').add([{
+  //   attributes: {
+  //     title: 'Open Templates'
+  //   },
+  //   className: 'fa fa-paperclip',
+  //   command: 'open-templates',//Open modal with templates which can be used when creating new page
+  //   id: 'open-templates'
+  // }, {
+  //   attributes: {
+  //     title: 'Open Pages'
+  //   },
+  //   className: 'fa fa-file-o',
+  //   command: 'open-pages',//Open modal listing your pages
+  //   id: 'open-pages'
+  // }, {
+  //   attributes: {
+  //     title: 'Save As Template'
+  //   },
+  //   className: 'fa fa-archive',
+  //   command: 'save-as-template',//Save page as template
+  //   id: 'save-as-template'
+  // }, {
+  //   attributes: {
+  //     title: 'Delete from IDB'
+  //   },
+  //   className: 'fa fa-trash-o',
+  //   command: 'delete-from-idb',//Delete open page or template
+  //   id: 'delete-from-idb'
+  // }]);
 
 };
